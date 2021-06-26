615dc1728c1e0fdb7cf772a3fbae605bdcc8b14a
DLBuesen <darren.buesen@gmail.com>
2021-06-26 15:44:54 +0200

    updated repository

--- project/windows10/docker/uninstall_docker_image.bat
+++ project/windows10/docker/uninstall_docker_image.bat
@@ -15,7 +15,7 @@ timeout 3
 echo Uninstalling the docker container image...
 echo.
 
-docker rmi dlbuesen/redox-active-film-distribution-reversible:v1.0.1-layer05
+docker rmi dlbuesen/redox-active-film-distribution-reversible:v1.0.1-layer04
 
 timeout 3
 echo Listing the installed docker images on the system...
@@ -28,7 +28,7 @@ timeout 3
 
 echo Docker image from repository
 echo "dlbuesen/redox-active-film-distribution-reversible"
-echo with tag "v1.0.1-layer05"
+echo with tag "v1.0.1-layer04"
 echo should not appear in the list of images above
 echo.
 
